97a641bc6cfdc3c112cc4c6c0cd78fa752586def
Bhargavi-kona <24331a05e1@mvgrce.edu.in>
2025-12-28 11:05:54 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,9 @@
+base=int(input("enter any number: "))
+exp=int(input("enter the exponent: "))
+power=1
+for i in range(exp):
+    power=power*base
+#power
+print(base,"to the power of",exp,"is:",power)
+
+
